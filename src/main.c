c71ca1d999bf5c077649db9b798523b8d3458bb4
patrick <patrickkevinmims@gmail.com>
2024-03-17 21:54:16 -0700

    src/stackADT.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
+#include <curl/curl.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "aes.h"
 #include "hdr.h"
 
 #define STACK_SIZE 20
@@ -10,6 +13,9 @@ int main(void)
 {
     stack_t stack = new_stack(STACK_SIZE);
     push(stack, 100);
+    peek(stack);
+
+    free(stack);
     
     return 0;
 }
--- src/stackADT.c
+++ src/stackADT.c
@@ -6,7 +6,6 @@
 
 // I think keeping the struct here is that the user can expand the struct by adding
 // new types.
-//
 struct Record 
 {
     char *name;
@@ -21,30 +20,30 @@ struct Stack
     struct Record record; 
 };
 
-stack_t new_stack(int size)
+stack_t new_stack(const int size)
 {
-  stack_t stack;
+    stack_t stack;
 
-  if((stack = malloc(sizeof(stack_t))) == NULL) exit(EXIT_FAILURE);
+    if((stack = malloc(sizeof(stack_t))) == NULL) exit(EXIT_FAILURE);
 
-  if((stack->data = malloc(size * sizeof(int))) == NULL)
-  {
-    free(stack); // Free stack before exit
-    exit(EXIT_FAILURE);
-  }
+    if((stack->data = malloc(size * sizeof(int))) == NULL)
+    {
+        free(stack);
+        exit(EXIT_FAILURE);
+    }
 
-  if((stack->name = malloc(size * sizeof(char *))) == NULL)
-  {
-    free(stack); // Free stack before exit
-    exit(EXIT_FAILURE);
-  }
+    if((stack->name = malloc(size * sizeof(char *))) == NULL)
+    {
+        free(stack);
+        exit(EXIT_FAILURE);
+    }
 
-  stack->ptr = 0;
-  stack->size = size;
+    stack->ptr = 0;
+    stack->size = size;
 
-  printf("Stack Created\n");
+    printf("Stack Created\n");
 
-  return stack;
+    return stack;
 }
 
 int isEmpty(stack_t stack)
@@ -66,20 +65,16 @@ void push(stack_t stack, const int item)
     }
 
     stack->data[stack->ptr] = item;
-//    stack->ptr++;
+    stack->ptr++;
 
-    printf("stack->[ %d ]\n", item);
+    printf("stack->[ %d ] - %d\n", item, stack->ptr);
+    peek(stack);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+void peek(stack_t stack)
+{
+    // we're trying to see the value of the node in which ptr points to. If I 
+    // try to read the value of the Node that ptr is currently pointing to it would
+    // be 0 - that's why I decrement the ptr variable. 
+    printf("peek -> %d\n", stack->data[stack->ptr--]);
+}
